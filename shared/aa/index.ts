6408b0080ff3286cc8ded0104f2a52df3f7ee34c
jabyl <jabyl.new@gmail.com>
2026-01-29 21:16:34 +0100

    modularized account abstraction infra, now natively deployed on Cronos Testnet

---
+++ shared/aa/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Account Abstraction Module
+ * 
+ * Provides gasless transaction support for Cronos chains via ERC-4337.
+ * 
+ * @module shared/aa
+ */
+
+export * from "./userop.js";
+export * from "./paymaster.js";
